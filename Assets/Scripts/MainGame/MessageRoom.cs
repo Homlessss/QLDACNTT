8f5580fa18cb6cbf2818fa3ba4822f32fdef8082
Homlessss <48959447+Homlessss@users.noreply.github.com>
2023-11-02 03:53:07 +0700

    Upload project

---
+++ Assets/Scripts/MainGame/MessageRoom.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Photon.Pun;
+
+public class MessageRoom : MonoBehaviour {
+
+    public Text textTitleMessage;
+    public Text nameCompetitor;
+    public GameObject butKick, butStart;
+
+    public void CallStart()
+    {
+        ResetDefault();
+    }
+
+    private void ResetDefault()
+    {
+        string textTitle = PhotonNetwork.CurrentRoom.Name;
+        textTitleMessage.text = textTitle + " (1/2)";
+        nameCompetitor.text = "COMPETITOR";
+        butKick.GetComponent<ButtonStatus>().Disable();
+        butStart.GetComponent<ButtonStatus>().Disable();
+    }
+
+    public void UpdateStatus()
+    {
+        if (Modules.listNamePlayer.Count < 2)
+        {
+            ResetDefault();
+            return;
+        }
+        if (PhotonNetwork.IsMasterClient)
+        {
+            textTitleMessage.text = PhotonNetwork.CurrentRoom.Name + " (2/2)";
+            nameCompetitor.text = Modules.listNamePlayer[1];
+            butKick.GetComponent<ButtonStatus>().Enable();
+            butStart.GetComponent<ButtonStatus>().Enable();
+        }
+        else
+        {
+            textTitleMessage.text = PhotonNetwork.CurrentRoom.Name + " (2/2)";
+            nameCompetitor.text = Modules.listNamePlayer[0];
+        }
+    }
+
+    public void ButtonKick()
+    {
+        Modules.networkManager.GetComponent<NetworkMagnager>().KickPlayer();
+        Modules.PlayAudioClipFree(Modules.audioButton);
+    }
+
+    public void ButtonStart()
+    {
+        Modules.networkManager.GetComponent<NetworkMagnager>().StartGame();
+    }
+
+    public void ButtonClose()
+    {
+        Modules.networkManager.GetComponent<NetworkMagnager>().CancelRoom();
+        Modules.PlayAudioClipFree(Modules.audioButton);
+    }
+}
